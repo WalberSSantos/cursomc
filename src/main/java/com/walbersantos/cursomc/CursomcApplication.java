c2ea5914d44168d7235764f754cc643a17ed6fe4
Walber Santos <walber_santos@hotmail.com>
2020-03-03 12:00:43 -0300

    Estado e Cidade

--- src/main/java/com/walbersantos/cursomc/CursomcApplication.java
+++ src/main/java/com/walbersantos/cursomc/CursomcApplication.java
@@ -8,18 +8,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.walbersantos.cursomc.domain.Categoria;
+import com.walbersantos.cursomc.domain.Cidade;
+import com.walbersantos.cursomc.domain.Estado;
 import com.walbersantos.cursomc.domain.Produto;
 import com.walbersantos.cursomc.repositories.CategoriaRepository;
+import com.walbersantos.cursomc.repositories.CidadeRepository;
+import com.walbersantos.cursomc.repositories.EstadoRepository;
 import com.walbersantos.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	//Instâncias das Dependências
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository; 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -28,12 +39,15 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		// Instância de Catetoria.
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		//Instância de Produto.
 		Produto p1 = new Produto(null,"Computador",2000.00);
 		Produto p2 = new Produto(null,"Impressora",800.00);
 		Produto p3 = new Produto(null,"Mouse",80.00);
+		
 		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
@@ -45,6 +59,26 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		//Instância Estado
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");		
+		
+		//Instância Cidade
+		Cidade c1 = new Cidade(null, "Urbelândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		//Relacionamento Estado X Cidades
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		//Relacionamento Cidades X Estado
+
+		
+		//Salvar os dados no banco - Estado/Cidade
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
 	}
 
